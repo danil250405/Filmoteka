36ee6739cccf048e3f2a239221b6b66d275621fe
glazweq <danilg250405@gmail.com>
2024-04-07 03:27:34 +0200

    finish style on pagination

--- src/main/resources/static/js/homeJS.js
+++ src/main/resources/static/js/homeJS.js
@@ -7,3 +7,35 @@ function setMovieData(name, description, img, ratingKp, ratingImdb, year) {
     document.querySelector('.modal-body .year').textContent = year;
 
 }
+
+
+const categoriesContainer = document.querySelector('.categories-container');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const progressIndicator = document.querySelector('.progress-indicator');
+const progressItems = document.querySelectorAll('.progress-item');
+
+let currentIndex = 0;
+const scrollAmount = categoriesContainer.offsetWidth;
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? progressItems.length - 1 : currentIndex - 1;
+    updateProgressIndicator();
+    categoriesContainer.scrollLeft -= scrollAmount;
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === progressItems.length - 1) ? 0 : currentIndex + 1;
+    updateProgressIndicator();
+    categoriesContainer.scrollLeft += scrollAmount;
+});
+
+function updateProgressIndicator() {
+    progressItems.forEach((item, index) => {
+        if (index === currentIndex) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+}
